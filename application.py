045351fb38b7c8e64375d2b899f7b42571de65af
rjrato <ricardo.jorge.rato@gmail.com>
2024-09-18 19:07:15 +0100

    first commit

---
+++ application.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, redirect, url_for, session
+from flask_bootstrap import Bootstrap5
+from dotenv import load_dotenv
+from datetime import date
+import os
+
+load_dotenv()
+
+application = Flask(__name__)
+application.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+Bootstrap5(application)
+
+
+@application.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    application.run(debug=True)
